4d854d23be11e5f09f93659275bc5b0496f4eda2
Shashank 20131A4201 <20131a4201@gvpce.ac.in>
2024-03-18 06:47:16 +0000

    First version deployed and Deckerized

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ from fastapi.responses import HTMLResponse
 import io
 from PIL import Image
 import os
-import cv2
 import shutil
-# from process import process_and_return
+from model import test_pnemonia
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory = "static"), name = "static")
@@ -27,30 +26,40 @@ def read_root(request: Request):
 async def upload_nib_scan(request: Request, scan: UploadFile):
     if scan.filename != '':
         # Check if the file extension is appropriate for NiB scans (e.g., .nii)
-        allowed_extensions = {'.nii', '.nii.gz', '.hdr', '.img'}
-        file_ext = os.path.splitext(scan.filename)[1].lower()
+        # allowed_extensions = {'.nii', '.nii.gz', '.hdr', '.img'}
+        # file_ext = os.path.splitext(scan.filename)[-1].lower()
 
-        if file_ext not in allowed_extensions:
-            return JSONResponse(content={"error": "Invalid file format. Only NIfTI or Analyze files are allowed."}, status_code=400)
+        # if file_ext not in allowed_extensions:
+        #     return JSONResponse(content={"error": "Invalid file format. Only NIfTI or Analyze files are allowed."}, status_code=400)
+
+        # scan_path = os.path.join(UPLOAD_FOLDER, scan.filename)
+        # with open(scan_path, "wb") as f:
+        #     shutil.copyfileobj(scan.file, f)
+
+        # Process the uploaded NiB scan
+        # try:
+        #     nib_scan = nib.load(scan_path)
+        #     # Perform additional processing on the NiB scan if needed
+        #     # For example: processed_data = process_nib_scan(nib_scan)
+        #     # ...
+        #     # return {"message": "NiB scan uploaded and processed successfully."}
+        #     print("NiB scan uploaded and processed successfully")
+        # except Exception as e:
+        #     # return JSONResponse(content={"error": f"Error processing NiB scan: {str(e)}"}, status_code=500)
+        #     print("Error processing NiB scan")
+
 
         scan_path = os.path.join(UPLOAD_FOLDER, scan.filename)
         with open(scan_path, "wb") as f:
             shutil.copyfileobj(scan.file, f)
 
-        # Process the uploaded NiB scan
-        try:
-            nib_scan = nib.load(scan_path)
-            # Perform additional processing on the NiB scan if needed
-            # For example: processed_data = process_nib_scan(nib_scan)
-            # ...
-            return {"message": "NiB scan uploaded and processed successfully."}
-        except Exception as e:
-            return JSONResponse(content={"error": f"Error processing NiB scan: {str(e)}"}, status_code=500)
+        prediction = test_pnemonia(scan_path)
+        prediction=round(prediction*100, 2)
+        return templates.TemplateResponse("index.html", {"request": request, "message": "CT Scan uploaded and processed successfully.", "Pred": prediction})
+       
+    return templates.TemplateResponse("index.html", {"request": request, "error": "No file uploaded."})
 
-    # return JSONResponse(content={"error": "No NiB scan selected."}, status_code=400)
-    #     return templates.TemplateResponse("index.html", {"request": request, "message": "Image uploaded and processed successfully.", "Pred": predictions})
-    # return templates.TemplateResponse("index.html", {"request": request, "error": "No image selected."})
-
+    
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,17 +1,26 @@
 from tensorflow.keras.models import load_model
 from process import process_nibabel
+import numpy as np
 
-test_image = "/workspace/3DCNN_Version1/study_0006.nii.gz"
+# test_image = "/workspace/3DCNN_Version1/study_0946.nii.gz"
 
-model=load_model("/workspace/3DCNN_Version1/3d_image_classification.keras")
-preprocessed_volume = process_nibabel(test_image)
-prediction = model.predict(np.expand_dims(preprocessed_volume, axis=0))[0]
-scores = [1 - prediction[0], prediction[0]]
+# model=load_model("/workspace/3DCNN_Version1/3d_image_classification.h5")
+# preprocessed_volume = process_nibabel(test_image)
+# prediction = model.predict(np.expand_dims(preprocessed_volume, axis=0))[0]
+# scores = [1 - prediction[0], prediction[0]]
 
-class_names = ["normal", "abnormal"]
+# class_names = ["Normal", "Abnormal"]
+# for score, name in zip(scores, class_names):
+#     print(
+#         "This model is %.2f percent confident that CT scan is %s"
+#         % ((100 * score), name)
+#     )
 
-for score, name in zip(scores, class_names):
-    print(
-        "This model is %.2f percent confident that CT scan is %s"
-        % ((100 * score), name)
-    )
+
+def test_pnemonia(file_path):
+    model=load_model("3d_image_classification.h5")
+    preprocessed_volume = process_nibabel(file_path)
+    prediction = model.predict(np.expand_dims(preprocessed_volume, axis=0))[0]
+    return prediction[0]
+
+# print(test_pnemonia(test_image))
